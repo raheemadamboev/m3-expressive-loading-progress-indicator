afbf998774beb3c3090a90281b6d90dc3c572c5a
raheem <raheemideadev@gmail.com>
2026-03-10 16:04:50 +0500

    Initial;

---
+++ app/src/main/java/xyz/teamgravity/m3expressiveloadingprogressindicator/MainActivity.kt
@@ -0,0 +1,47 @@
+package xyz.teamgravity.m3expressiveloadingprogressindicator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.m3expressiveloadingprogressindicator.ui.theme.M3ExpressiveLoadingProgressIndicatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            M3ExpressiveLoadingProgressIndicatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    M3ExpressiveLoadingProgressIndicatorTheme {
+        Greeting("Android")
+    }
+}
